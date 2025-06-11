cbea6832a5c413ec46920313b0241ea8df4f17fd
sleepytyz <vietanhnguyen172008@gmail.com>
2025-06-11 16:59:47 +0700

    SANPHAMDAO

---
+++ src/DAO/SanPhamDAO.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.*;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import model.SanPham;
+import service.DBconnect;
+import java.sql.Date;
+
+/**
+ *
+ * @author ACER
+ */
+public class SanPhamDAO {
+    public Object getRow(SanPham sp) {
+        int maSP = sp.getMaSP();
+        String loai = sp.getLoai();
+        String ten = sp.getTenSP();
+        Date ngaydathang = sp.getNgaydathang();
+        int giaban = sp.getGiaban();
+        Object[] row = new Object[]{maSP,loai,ten,ngaydathang,giaban};
+        return row;
+    }
+    
+     public List<SanPham> getALL() {
+        List<SanPham> listSP = new ArrayList<>();
+        String sql = "SELECT * FROM SanPham";
+        try {
+            Connection con = DBconnect.getConnection();
+            Statement stm = con.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+            while (rs.next()) {
+                int maSP = rs.getInt(1);
+                String loai = rs.getString(2);
+                String ten = rs.getString(3);
+                Date ngaydathang = rs.getDate(4);
+                int giaBan = rs.getInt(5);
+                SanPham sp = new SanPham(maSP, loai, ten,ngaydathang,giaBan);
+                listSP.add(sp);
+            }
+        } catch (Exception e) {
+        }
+        return listSP;
+    }
+     
+      public boolean getadd(SanPham sp) {
+        String sql = "INSERT INTO SanPham(Ma, Loai, TenSP, NgayDatHang, GiaBan) VALUES(?, ?, ?, ?, ?)";
+        try {
+            Connection con = DBconnect.getConnection();
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setInt(1, sp.getMaSP());
+            stm.setString(2, sp.getLoai());
+            stm.setString(3, sp.getTenSP());
+            stm.setDate(4, sp.getNgaydathang());
+            stm.setInt(5, sp.getGiaban());
+            if (stm.executeUpdate()>0) {
+                System.out.println("Them thanh cong");
+                return true;
+            }
+        } catch (Exception e) {
+        }
+        return false;
+    }
+    
+    public boolean UpdateSP(SanPham sp) {
+        String sql = "UPDATE SanPham SET TenSP = ?, Loai = ?, NgayDatHang = ?, GiaBan = ?, where MaSP = ?";
+        try {
+            Connection con = DBconnect.getConnection();
+            PreparedStatement pstm = con.prepareStatement(sql);
+            pstm.setString(1, sp.getLoai());
+            pstm.setString(2, sp.getTenSP());
+            pstm.setDate(3, sp.getNgaydathang());
+            pstm.setInt(4, sp.getGiaban());
+            if (pstm.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+    public boolean UpdateSP(view.SanPham sp) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public boolean getadd(view.SanPham sp) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
+
